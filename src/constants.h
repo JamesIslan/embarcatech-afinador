bedbe86490da2339190e560cdae5fe4f46b39b64
Joamerson Islan <joamersonislan38@gmail.com>
2025-02-11 09:40:52 -0300

    refactor: constantes movidas para arquivo separado

---
+++ src/constants.h
@@ -0,0 +1,17 @@
+#include "env.h"
+
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS 17
+#define PIN_SCK 18
+#define PIN_MOSI 19
+
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
---
+++ src/env-example.h
@@ -0,0 +1,4 @@
+// Renomeie este arquivo para "env.h"
+
+#define WIFI_SSID "SuaRede"
+#define WIFI_SENHA "SuaSenha"

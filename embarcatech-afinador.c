503193a138888f4061bef68cbdd2ab78bf84b597
Joamerson Islan <joamersonislan38@gmail.com>
2025-02-11 09:44:35 -0300

    refactor: implementado arquivo de credenciais

--- embarcatech-afinador.c
+++ embarcatech-afinador.c
@@ -44,7 +44,7 @@ int main() {
   cyw43_arch_enable_sta_mode();
 
   printf("Connecting to Wi-Fi...\n");
-  if (cyw43_arch_wifi_connect_timeout_ms("JCDA", "josemario1515", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+  if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_SENHA, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
     printf("failed to connect.\n");
     return 1;
   } else {

0d402969aa2c351b55bc55d719736b5e628e2c98
Joamerson Islan <joamersonislan38@gmail.com>
2025-02-15 10:58:19 -0300

    feat: melhorado precisão do microfone

--- src/mic/mic.h
+++ src/mic/mic.h
@@ -1,12 +1,13 @@
 #include "../fft/kiss_fftr.h"
 #include "hardware/dma.h"
 
+#define MIC_CANAL_ADC 2
+#define MIC_GPIO 28
+
 #define DIVISOR_CLOCK 960
 #define FSAMP 50000
 #define FREQUENCIA_MAXIMA_CAPTURA 500
-#define MIC_CANAL_ADC 2
-#define MIC_GPIO 28
-#define N_AMOSTRAS 4000
+#define N_AMOSTRAS 6500
 
 extern volatile float max_freq;
 extern kiss_fftr_cfg cfg_fftr;
